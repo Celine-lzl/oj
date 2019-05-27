2fda3020fc4c1e458d12ad0e69c2f16cb96b897b
qigemingzizhememafan <43529959+qigemingzizhememafan@users.noreply.github.com>
2019-05-27 16:44:05 +0800

    分糖果，找一个数组中出现n/2次的数

---
+++ 5_27/src/com/lzl/Main.java
@@ -0,0 +1,56 @@
+package com.lzl;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+import java.util.Stack;
+
+public class Main{
+//**************      day06 A、B、C三个人是好朋友，分糖果，已知A+B、A-B等    ******************************
+//    public static void main(String[] args){
+//        Scanner scanner = new Scanner(System.in);
+//        int a1,a2,a3,a4;
+//        while(scanner.hasNext()){
+//            a1 = scanner.nextInt();
+//            a2 = scanner.nextInt();
+//            a3 = scanner.nextInt();
+//            a4 = scanner.nextInt();
+//            int B = (a2 + a4) / 2;
+//            int A = (a1 + a3) / 2;
+//            int C = a4 - B;
+//            if(A - B == a1 && B - C == a2 && A + B == a3 && B + C == a4){
+//                System.out.println(A+" "+B+" "+C);
+//            }else{
+//                System.out.println("No");
+//            }
+//        }
+//    }
+
+//*********************     day06  n个数里求出现次数为n/2次的数     ************************************
+//public static void main(String[] args){
+//    Scanner scanner = new Scanner(System.in);
+//    String str;
+//    while(scanner.hasNext()){
+//        int count = 0;
+//        str = scanner.nextLine();
+//        String[] data = str.split("("+" "+")");
+//        int[] data1 = new int[data.length];
+//        for(int i = 0; i < data1.length; i++){
+//            data1[i] = Integer.valueOf(data[i]);
+//        }
+//        int a = data1[0];
+//        for(int j = 0; j < data1.length; j++){
+//            if(data1[j] == a){
+//                count++;
+//            }else if(count > 0){
+//                count --;
+//            }else{
+//                a = Integer.valueOf(data[j]);
+//            }
+//        }
+//        System.out.println(a);
+//    }
+//}
+
+
+
+}
